90c99cfbf2a0e76e2c37c58e008aa3f5ce5c497c
Noell Biju Michael <115487353+NoellMichael@users.noreply.github.com>
2023-05-10 07:10:41 +0530

    Add files via upload

--- splash.dart
+++ splash.dart
@@ -1,44 +1,36 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:noell/home.dart';
 
-import 'main.dart';
-
-class Splash extends StatefulWidget {
-  const Splash({super.key});
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
-  State<Splash> createState() => _SplashState();
+  State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashState extends State<Splash> {
+class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+              builder: (context) => const MyHomePage(
+                    title: 'gfg',
+                  )));
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomRight,
-            colors: [
-              Color.fromARGB(255, 13, 70, 78),
-              Color.fromARGB(255, 74, 204, 221),
-            ],
-          ),
-        ),
-        child: Column(children: [
-          Image.asset(
-            'android/assets/image/cyan.jpg',
-            height: 300.0,
-            width: 300.0,
-          ),
-        ]),
-      ),
-    );
+        body: Center(
+            child: Container(
+                color: const Color.fromARGB(255, 11, 100, 89),
+                height: MediaQuery.of(context).size.height,
+                child: const Image(
+                    image: AssetImage("android/assets/image/logoblack.png")))));
   }
 }
